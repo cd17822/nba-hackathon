18f038d27eff0adeea1128e00f48460c4398edfd
pranavbadami <pranavbadami@gmail.com>
2016-09-24 15:24:09 -0400

    no pandas comp

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask import render_template
-import pandas as pd
-import math
-from scipy import stats
-import json
+# import pandas as pd
+# import math
+# from scipy import stats
+# import json
 
 app = Flask(__name__) 
-data = pd.read_csv("data/game.csv")
-global OFFENSE
-OFFENSE = data[((data["BX"] <= 47) & (data["Period"].isin([1,2]))) | ((data["BX"] >= 47) & (data["Period"].isin([3,4])))]
-OFFENSE["RevClock"] = OFFENSE.apply(lambda x: 720-math.ceil(x["Game_Clock"]), axis=1)
-OFFENSE["Clock"] = OFFENSE.apply(lambda x: 720*(x["Period"]-1) + x["RevClock"], axis=1)
+# data = pd.read_csv("data/game.csv")
+# global OFFENSE
+# OFFENSE = data[((data["BX"] <= 47) & (data["Period"].isin([1,2]))) | ((data["BX"] >= 47) & (data["Period"].isin([3,4])))]
+# OFFENSE["RevClock"] = OFFENSE.apply(lambda x: 720-math.ceil(x["Game_Clock"]), axis=1)
+# OFFENSE["Clock"] = OFFENSE.apply(lambda x: 720*(x["Period"]-1) + x["RevClock"], axis=1)
 #misc
 SECONDS_IN_PERIOD = 12 * 60
 GAME_IS_OVER = False
@@ -84,27 +84,30 @@ def player_dist(row, playerID=PLAYER, teamID=1610612744):
                 distance = d
     return closest
 
+# @app.route('/defense/<last_name>')
+# def defense(last_name):
+#     print "in here"
+#     global PLAYER
+#     PLAYER = last_name
+#     offense = OFFENSE
+#     offense["Closest"] = offense.apply(lambda x: player_dist(x), axis=1)
+#     clock = offense.groupby("Clock")
+#     valid = clock.filter(lambda x: len(x) <= 30).groupby("Clock")
+#     per_sec = valid["Closest"].agg(stats.mode)
+#     closest = per_sec.to_frame()
+#     closest["Second"] = closest.index 
+#     closest["Minute"] = closest.apply(lambda x: math.ceil(x["Second"]/60), axis=1)
+#     closest["Name"] = closest.apply(lambda x: x["Closest"][0][0], axis=1)
+#     closest_by_min = closest.groupby("Minute")
+#     per_min = closest_by_min["Name"].agg(stats.mode)
+#     closest_min = per_min.to_frame()
+#     closest_min["Minute"] = closest_min.index
+#     closest_min["Name"] = closest_min.apply(lambda x: x["Name"][0][0], axis=1)
+#     l = list(closest_min.sort_values("Minute")["Name"].values)
+#     return '{ "defense":' + json.dumps(smooth_list(l)) + '}'
 @app.route('/defense/<last_name>')
 def defense(last_name):
-    print "in here"
-    global PLAYER
-    PLAYER = last_name
-    offense = OFFENSE
-    offense["Closest"] = offense.apply(lambda x: player_dist(x), axis=1)
-    clock = offense.groupby("Clock")
-    valid = clock.filter(lambda x: len(x) <= 30).groupby("Clock")
-    per_sec = valid["Closest"].agg(stats.mode)
-    closest = per_sec.to_frame()
-    closest["Second"] = closest.index 
-    closest["Minute"] = closest.apply(lambda x: math.ceil(x["Second"]/60), axis=1)
-    closest["Name"] = closest.apply(lambda x: x["Closest"][0][0], axis=1)
-    closest_by_min = closest.groupby("Minute")
-    per_min = closest_by_min["Name"].agg(stats.mode)
-    closest_min = per_min.to_frame()
-    closest_min["Minute"] = closest_min.index
-    closest_min["Name"] = closest_min.apply(lambda x: x["Name"][0][0], axis=1)
-    l = list(closest_min.sort_values("Minute")["Name"].values)
-    return '{ "defense":' + json.dumps(smooth_list(l)) + '}'
+    return '{ "defense":["JR Smith", "Kyrie Irving", "Kyrie Irving", "Kyrie Irving", "JR Smith", "JR Smith", "Kyrie Irving", "Kyrie Irving", "Kyrie Irving", "Tristan Thompson", "Matthew Dellavedova", 0, 0, "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Richard Jefferson", "LeBron James", "Kyrie Irving", "Kyrie Irving", "Tristan Thompson", "JR Smith", "Kyrie Irving", "Kyrie Irving", "Kyrie Irving", 0, 0, 0, "LeBron James", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Iman Shumpert", "Kyrie Irving", "JR Smith", "JR Smith", 0, 0, 0]}'
 
 def initPtsGlobals():
     global DATA_INDEX, GAME_IS_OVER, IN_GAME, DATA, LAST_DATAPOINT, PTS
