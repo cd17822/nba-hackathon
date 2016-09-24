51203584da2a7ab5673747da9cd451f372818c6b
pranavbadami <pranavbadami@gmail.com>
2016-09-24 15:18:02 -0400

    added pandas in

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from flask import render_template
-app = Flask(__name__)
+import pandas as pd
+import math
+from scipy import stats
+import json
 
+app = Flask(__name__) 
+data = pd.read_csv("data/game.csv")
+global OFFENSE
+OFFENSE = data[((data["BX"] <= 47) & (data["Period"].isin([1,2]))) | ((data["BX"] >= 47) & (data["Period"].isin([3,4])))]
+OFFENSE["RevClock"] = OFFENSE.apply(lambda x: 720-math.ceil(x["Game_Clock"]), axis=1)
+OFFENSE["Clock"] = OFFENSE.apply(lambda x: 720*(x["Period"]-1) + x["RevClock"], axis=1)
 #misc
 SECONDS_IN_PERIOD = 12 * 60
 GAME_IS_OVER = False
@@ -36,9 +45,66 @@ def pts(last_name):
 def home():
     return render_template('index.html')
 
+def smooth_list(l):
+    for i in range(1, len(l)-1):
+        before = l[i-1]
+        now = l[i]
+        after = l[i+1]
+        if before==after:
+            if now!= before:
+                now = before
+                l[i] = now
+    return l
+
+def player_dist(row, playerID=PLAYER, teamID=1610612744):
+# def player_dist(row):
+    opposing_players = []
+    player = []
+    closest = None
+    for i in range(1,11):
+        name = "Player"+str(i)
+        pid = name+".1"
+        tid = name+"Team"
+        x = "P"+str(i)+"X"
+        y = "P"+str(i)+"Y"
+        if row[tid] != teamID:
+            opposing_players.append([row[x], row[y], row[name]])
+        try:
+            if (playerID in row[name]) and (row[tid] == teamID):
+                player = [row[x],row[y]]
+        except TypeError:
+            pass
+    distance = float("inf")
+    if len(player) == 2:
+        for p in opposing_players:
+            d = (p[0]-player[0])*(p[0]-player[0]) + (p[1]-player[1])*(p[1]-player[1])
+            d = math.sqrt(d)
+            if d < distance:
+                closest = p[2]
+                distance = d
+    return closest
+
 @app.route('/defense/<last_name>')
 def defense(last_name):
-    return "hi"
+    print "in here"
+    global PLAYER
+    PLAYER = last_name
+    offense = OFFENSE
+    offense["Closest"] = offense.apply(lambda x: player_dist(x), axis=1)
+    clock = offense.groupby("Clock")
+    valid = clock.filter(lambda x: len(x) <= 30).groupby("Clock")
+    per_sec = valid["Closest"].agg(stats.mode)
+    closest = per_sec.to_frame()
+    closest["Second"] = closest.index 
+    closest["Minute"] = closest.apply(lambda x: math.ceil(x["Second"]/60), axis=1)
+    closest["Name"] = closest.apply(lambda x: x["Closest"][0][0], axis=1)
+    closest_by_min = closest.groupby("Minute")
+    per_min = closest_by_min["Name"].agg(stats.mode)
+    closest_min = per_min.to_frame()
+    closest_min["Minute"] = closest_min.index
+    closest_min["Name"] = closest_min.apply(lambda x: x["Name"][0][0], axis=1)
+    l = list(closest_min.sort_values("Minute")["Name"].values)
+    return '{ "defense":' + json.dumps(smooth_list(l)) + '}'
 
 def initPtsGlobals():
     global DATA_INDEX, GAME_IS_OVER, IN_GAME, DATA, LAST_DATAPOINT, PTS
@@ -164,5 +230,4 @@ def updateDataToIndex(index, data):
     DATA_INDEX = index
 
 if __name__ == '__main__':
-    print "yo"
     main()
